503460a08a3e06423f8a1265ec9b1b23e64d57ab
jhilton642 <joshua.hilton@att.net>
2020-10-26 14:18:02 -0500

    Add project files.

---
+++ Final14/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Final14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 2000; i++)
+            {
+                Console.WriteLine(i);
+                /*i was born in January so i already am incrementing by months
+                   if I was born Feb 2 my loop would look like this: for(int i = 2; i<=2000;i+=2){} */
+            }
+        }
+    }
+}
